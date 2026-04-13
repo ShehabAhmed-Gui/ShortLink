3daa06d23c42b7efde39926a4680af2ba318a4a9
Shehab <shehabahmed.guii@gmail.com>
2026-04-13 13:13:34 +0200

    fix: use api prefix for api views

--- shortlink/urls.py
+++ shortlink/urls.py
@@ -3,7 +3,7 @@ from .views.api_views import UrlAPIView
 from .views.redirect_views import RedirectView
 
 urlpatterns = [
-    path('shortlink/', UrlAPIView.as_view()),
-    path('shortlink/<str:short_url>/', UrlAPIView.as_view()),
-    path('shortlink/<str:short_url>', RedirectView.as_view()),
+    path('api/shortlink/', UrlAPIView.as_view()),
+    path('api/shortlink/<str:short_url>/', UrlAPIView.as_view()),
+    path('shortlink/<str:short_url>/', RedirectView.as_view()),
 ]
