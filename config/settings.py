70c70b538942a8b717f0cb9ffa0544d4fb6e25bd
Shehab <shehabahmed.guii@gmail.com>
2026-04-13 06:45:41 +0200

    refactor: change db name to new project name

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'urlshortner',
+        'NAME': 'shortlink',
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': 'localhost',
